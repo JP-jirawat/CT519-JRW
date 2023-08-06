2f492ad4a66162cdd4e33f73e31a00a0add3b5aa
Jirawat MTTS <jirawatmtts@MacBook-Pro-khxng-Jirawat.local>
2023-08-06 13:00:26 +0700

    jrw

---
+++ frontend/src/components/About/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+    return (
+        <>
+            <h1>นาย จิรวัฒน์ นพชัยอำนวยโชค</h1>
+            <div>
+                <p>
+                   รหัสประจำตัว 65130029
+                </p>
+            </div>
+            <div>
+                <img src="https://scontent.fbkk2-8.fna.fbcdn.net/v/t39.30808-6/342380657_247656284461489_983208231865697139_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG2b2oKicTd6uIeJCT9mr5pLnYmwH92C9wudibAf3YL3OVobJQHz10zZop-tfr6xEJVNB9Z6-DgF-f3tG4g7jEF&_nc_ohc=Rj6T3jAi0SEAX9ASmD3&_nc_ht=scontent.fbkk2-8.fna&oh=00_AfCp5gGC_1EHUBVpntgMO7CotBpVBY_esTeWzpTXWCJOYA&oe=64D35E20"/>
+            </div>
+        </>
+    )
+}
+export default About
